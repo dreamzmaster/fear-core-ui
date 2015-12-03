92f775ff19aec76975c36e11b8e1188f300c3568
kevinlambert <kevin@becreative.technology>
2015-12-03 12:25:17 +0000

    rename

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ var fearCoreUiEntryPoint = require.resolve('fear-core-ui');
 
 var sassDir = path.dirname(fearCoreUiEntryPoint) + '/app/sass';
 var assetsDir = path.dirname(fearCoreUiEntryPoint) + '/app/assets';
-var assetsImagesDir = path.dirname(fearCoreUiEntryPoint) + '/app/assets/images';
-var assetsFontsDir = path.dirname(fearCoreUiEntryPoint) + '/app/assets/fonts';
+var assetImageDir = path.dirname(fearCoreUiEntryPoint) + '/app/assets/images';
+var assetFontDir = path.dirname(fearCoreUiEntryPoint) + '/app/assets/fonts';
 
 module.exports = {
     sassPaths: [sassDir, bourbon.includePaths],
-    assetsPaths: assetsDir,
-    assetsImagesPaths: assetsImagesDir,
-    assetsFontsPaths: assetsFontsDir
+    assetPaths: assetsDir,
+    assetImagePaths: assetImageDir,
+    assetFontPaths: assetFontDir
 };
